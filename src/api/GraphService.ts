0066405660c844281a9aac136715c068db2e688f
Yatheesh Nagella <yatheeshnagella17@gmail.com>
2025-04-26 17:44:55 -0500

    action-blueprint-graph-get

--- src/api/GraphService.ts
+++ src/api/GraphService.ts
@@ -1,5 +1,5 @@
 export const fetchGraphData = async () => {
-    const response = await fetch('http://localhost:3000/api/v1/test/actions/blueprints/example/graph');
+    const response = await fetch('http://localhost:3000/api/v1/123/actions/blueprints/bp_456/bpv_123/graph');
     if (!response.ok) {
       throw new Error('Failed to fetch graph data');
     }
